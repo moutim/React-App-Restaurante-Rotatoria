e9d348640bc394bdcadb30c4f4ea85f9395c0ad7
Vitor Moutim <moutimg@gmail.com>
2023-07-29 22:49:21 -0300

    feat: coloca logo do restaurante rotatoria no centro da navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './navbar.css';
 import { Link } from 'react-router-dom';
+const logo = require('../../assets/logo.png');
 
 function Navbar() {
   return (
@@ -9,7 +10,7 @@ function Navbar() {
         <ul>
           <li><Link to='/registrar/pedido'>REGISTRAR PEDIDO</Link></li>
           <li><Link to='/registrar/cliente'>REGISTRAR CLIENTE</Link></li>
-          <li className='logo'><Link to='/'><img src='https://logodownload.org/wp-content/uploads/2017/02/psg-logo-escudo-paris-saint-germain.png' alt='Logo Restaurante Rotatoria'/></Link></li>
+          <li className='logo'><Link to='/'><img src={logo} alt='Logo Restaurante Rotatoria'/></Link></li>
           <li><Link to='/vendas'>TODOS OS PEDIDOS</Link></li>
           <li><Link to='/clientes'>TODOS OS CLIENTES</Link></li>
         </ul>
