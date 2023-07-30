3611f0fe306a8b8075002d5ddfa5e9da3c22c9d3
Vitor Moutim <moutimg@gmail.com>
2023-07-29 22:17:41 -0300

    fix: typo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,8 +10,8 @@ function Navbar() {
           <li><Link to='/registrar/pedido'>REGISTRAR PEDIDO</Link></li>
           <li><Link to='/registrar/cliente'>REGISTRAR CLIENTE</Link></li>
           <li className='logo'><Link to='/'><img src='https://logodownload.org/wp-content/uploads/2017/02/psg-logo-escudo-paris-saint-germain.png' alt='Logo Restaurante Rotatoria'/></Link></li>
-          <li><Link to='/vendas'>TODAS AS VENDAS</Link></li>
-          <li><Link to='/clientes'>TODOS OS PEDIDOS</Link></li>
+          <li><Link to='/vendas'>TODOS OS PEDIDOS</Link></li>
+          <li><Link to='/clientes'>TODOS OS CLIENTES</Link></li>
         </ul>
       </nav>
     </header>
