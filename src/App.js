f43b2fdbd3757dbd683a162d0c162d36cf562499
Vitor Moutim <moutimg@gmail.com>
2023-07-30 23:01:21 -0300

    fix: corrige nome das paginas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Cliente from './pages/cliente/Cliente';
 import Navbar from './components/navbar/Navbar';
 import './components/container/container.css';
+import Vendas from './pages/vendas/Vendas';
+import Clientes from './pages/clientes/Clientes';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Navbar/>
       <main className='main'>
         <Routes>
-          <Route path='/clientes' Component={Cliente}/>
+          <Route path='/clientes' Component={Clientes}/>
+          <Route path='/vendas' Component={Vendas}/>
         </Routes>
       </main>
     </BrowserRouter>
